653d5f4bba90577654a00288aca41e49e2c2c0cd
김주은 <bbbvovo12323@gmail.com>
2021-06-19 21:44:15 +0900

    메인 액티비티 에딧텍스트 1->2 이동 엔터버튼 연동 에딧텍스트 2에서 엔터버튼으로 추가가능 단어리스트 레이아웃 하단 버튼&리스트 길이 조절

--- app/src/main/java/com/example/swordcard/MainActivity.java
+++ app/src/main/java/com/example/swordcard/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 
 import java.util.List;
 import java.util.Vector;
+
+import static android.view.KeyEvent.ACTION_DOWN;
 
 public class MainActivity extends AppCompatActivity {
     List<WordEntry> list = new Vector<WordEntry>();
@@ -20,13 +23,52 @@ public class MainActivity extends AppCompatActivity {
     private EditText mMeanInput;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WordModule a = new WordModule(MainActivity.this);
         list = a.getAllWords(); // db에서 단어 데이터를 받아온다.
         mWordInput = (EditText) findViewById(R.id.wordedit);
         mMeanInput = (EditText) findViewById(R.id.meanedit);
+
+        mWordInput.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction()==KeyEvent.ACTION_DOWN) && keyCode==KeyEvent.KEYCODE_ENTER) {
+                    mMeanInput.requestFocus();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        mMeanInput.setOnKeyListener(new View.OnKeyListener() {
+
+            @Override
+            public boolean onKey(View v, int keycode, KeyEvent event) {
+                String word = mWordInput.getText().toString();
+                String mean = mMeanInput.getText().toString();
+
+                if((event.getAction() == ACTION_DOWN) && (keycode == KeyEvent.KEYCODE_ENTER)) {
+
+                    if(word.length() <= 0 || mean.length() <= 0){
+                        Toast.makeText(getApplicationContext(), "다시 입력해주세요.",Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        WordEntry entry = new WordEntry(word,mean);
+                        a.addWord(entry);
+                        Toast.makeText(getApplicationContext(), "단어가 추가되었습니다!!",Toast.LENGTH_LONG).show();
+                        mWordInput.setText(null);
+                        mMeanInput.setText(null);
+                    }
+
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
 
         // 단어 수동입력 모드
         btn_enter = (Button) findViewById(R.id.enter);
@@ -35,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String word = mWordInput.getText().toString();
                 String mean = mMeanInput.getText().toString();
+
 
                 if(word.length() <= 0 || mean.length() <= 0){
                     Toast.makeText(getApplicationContext(), "다시 입력해주세요.",Toast.LENGTH_LONG).show();
@@ -47,8 +90,6 @@ public class MainActivity extends AppCompatActivity {
                     mMeanInput.setText(null);
                 }
 
-
-                Intent intent= new Intent(MainActivity.this,QuizActivity.class);
             }
         });
 
