2a571c413961555157c82aa9f138fd2eb286aa11
poobool-koon <mygm1302@gmail.com>
2021-06-21 00:31:05 +0900

    Google Login

--- app/src/main/java/com/example/swordcard/WordListActivity.java
+++ app/src/main/java/com/example/swordcard/WordListActivity.java
@@ -3,8 +3,10 @@ package com.example.swordcard;
 import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -24,6 +26,12 @@ import com.example.swordcard.R;
 import com.example.swordcard.WordEntry;
 import com.example.swordcard.WordModule;
 import com.example.swordcard.WordsCloud;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,7 +44,8 @@ public class WordListActivity extends AppCompatActivity {
     private List<WordEntry> show_list;
     private WordsCloud wc;
     private WordModule wordModule;
-
+    int RC_SIGN_IN = 9002;
+    GoogleSignInClient mGoogleSignInClient;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,9 +65,9 @@ public class WordListActivity extends AppCompatActivity {
         sync_btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                wc.upload("test");
-                wc.download("test");
-                finish();
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+//                finish();
             }
         });
 
@@ -73,6 +82,62 @@ public class WordListActivity extends AppCompatActivity {
                 deleteAlert("다음 단어를 삭제하시겠습니까?",show_list.get(position).toString(), position);
             }
         });
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println("on Activity Result...");
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        System.out.println("HANDLE SIGN IN RESULT!");
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            loginAlert("로그인 완료",account.getDisplayName()+"의 단어장을 가져옵니다.", "ID"+account.getId());
+//            System.out.println("ID:");
+            // Signed in successfully, show authenticated UI.
+        } catch (ApiException e) {
+            System.out.println("HANDLE SIGN EXCEPTION...");
+            System.out.println( "signInResult:failed code=" + e.getStatusCode());
+//            updateUI(null);
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+        }
+    }
+
+    private void loginAlert(String title, String message, String ID)
+    {
+//        WordModule
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setCancelable(true)
+                .setPositiveButton("확인", new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        wc.upload(ID);
+                        wc.download(ID);
+                        dialog.dismiss();
+                        finish();
+                    }
+                })
+                .show();
     }
     private void deleteAlert(String title, String message,int position)
     {
@@ -101,4 +166,5 @@ public class WordListActivity extends AppCompatActivity {
                 })
                 .show();
     }
+
 }
